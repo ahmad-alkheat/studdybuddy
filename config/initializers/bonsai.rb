707994a997d530630a86804a6739d0c39623590d
Ahmad Al-kheat <Ahmad@Ahmads-MacBook-Pro.local>
2014-12-30 00:30:12 -0500

    added search feature

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,3 +1,3 @@
 if ENV[‘BONSAI_URL’]
-     Searchkick.client = Elasticsearch::Client.new({url: ENV[‘BONSAI_URL’], logs: true})
+     Searchkick.client = Elasticsearch::Client.new({url: ENV['https://twwyawq8:zrihxyxrsx50i71m@fig-4052474.us-east-1.bonsai.io'], logs: true})
 end

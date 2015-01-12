67d22ae840e5c7f53b1668d1a5186c4a3cd75007
Ahmad <wisamfaithful@gmail.com>
2015-01-12 11:56:32 -0500

    forgot password email

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,8 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
+config.action_mailer.default_url_options = { :host => 'https://vast-badlands-7913.herokuapp.com/' }
+
 ActionMailer::Base.smtp_settings = {
   :user_name => 'RailsOuter',
   :password => 'ahmad0000',

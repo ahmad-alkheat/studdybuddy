d7f1f49b43b23f70a3c0766c7241b43ccfce1050
Ahmad <wisamfaithful@gmail.com>
2015-02-23 21:37:31 -0500

    style

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,22 @@
 class User < ActiveRecord::Base
 
- has_attached_file :avatar,
+  validates :name, presence: true
+  has_attached_file :avatar,
   :styles => { :medium => "300x300>", :thumb => "100x100>" },
   :storage => :s3,
   :bucket => 'photosstuddybuddy'
 
- validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 	has_many :posts,  dependent: :destroy
 
 	def self.from_omniauth(auth)
-    	where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    		user.email = auth.info.email
-    		user.password = Devise.friendly_token[0,20]
-    		user.name = auth.info.name 
-        user.image_string = auth.info.image 
-  		end
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    	user.email = auth.info.email
+    	user.password = Devise.friendly_token[0,20]
+    	user.name = auth.info.name 
+      user.image_string = auth.info.image 
+  	end
 	end
   
   devise :database_authenticatable, :registerable,

6a99a5e5d58e01cfcf9b57dc9b249e12f4a442fe
Ahmad <wisamfaithful@gmail.com>
2015-02-25 16:42:20 -0500

    updated course list

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,10 +1,10 @@
 module PostsHelper
 	def course_names
-		["CISC","BIOL","PSYC"]
+		["CISC","BIOL","PSYC", "ANAT","ANCH","AODA","APSC","ARAB","ARTC","ARTF","ARTH","BCHM","BIOL","BIOM","BIOT","BISC","BMED","CDNS","CHEE","CHEM","CHIN","CISC","CIVL","CLAS","CMC","CMPE","COCA","COGS","COMM","COMP","CWRI","DEVS","DRAM","ECON","EDST","EDUC","ELEC","ENGL","FILM","FREN","GEOL","GPHY","GREK","GRMN","HIST","HLTH","LAW","LING","LISC","MATH","MBIO","MUSC","NURS","PHAR","PHIL","PHYS","SPAN","WRTI"]
 	end		
 
 	def course_numbers
-		[121,221,232,343,231,432]
+		(1..500).to_a
 	end
 
 	def gender
